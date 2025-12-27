b29003cfc512675e7355580e63462012d4454b2a
kr-mohit06 <mohit.kumar@accelleron-industries.com>
2025-12-27 12:37:35 +0530

    update migration

--- Migrations/20251227070641_UpdateTrachers.Designer.cs
+++ Migrations/20251227070641_UpdateTrachers.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SMS.API.Data;
@@ -11,9 +12,11 @@ using SMS.API.Data;
 namespace SMS.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251227070641_UpdateTrachers")]
+    partial class UpdateTrachers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251227070641_UpdateTrachers.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMS.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTrachers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId1",
+                table: "Teachers",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_UserId1",
+                table: "Teachers",
+                column: "UserId1",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Teachers_Users_UserId1",
+                table: "Teachers",
+                column: "UserId1",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Teachers_Users_UserId1",
+                table: "Teachers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Teachers_UserId1",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Teachers");
+        }
+    }
+}
